4b0bff6098b82535076d0dcc07bb658fd340c9e3
tareqzoubii <tareqzoubi4@gmail.com>
2022-10-31 12:43:03 -0700

    feat: function2

--- api/hello.py
+++ api/hello.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+# from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
 
@@ -7,6 +7,6 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    self.wfile.write(str(date).encode())
+    message = "Hello User"
+    self.wfile.write(str(message).encode())
     return
