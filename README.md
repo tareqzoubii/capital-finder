66eb05b335f4cd5249315a224b9831747db6e33c
tareqzoubii <tareqzoubi4@gmail.com>
2022-10-31 17:37:12 -0700

    feat: solvingLab

---
+++ README.md
@@ -0,0 +1,22 @@
+# Lab 13 
+# Created by Tareq Zoubii
+# Project Name: Capital Finder
+
+
+## Lab Requriments:
+#### Create a serverless function following Vercel's get-started directions that handles two kinds of queries: The serverless function should handle a GET http request with a given country name that responds with a string with the form The capital of X is Y, Example: when you enter a name of country it will give you the capital of that country and the opposite
+
+## Resourses:
+[VercelCli](https://vercel.com/docs/concepts/deployments/overview#vercel-cli)
+[Vercel](https://www.youtube.com/watch?v=Fx3ZGy-mbV4)
+
+## Api Used:
+[API](https://restcountries.com/#rest-countries)
+
+## Pull Request:
+[PR]()
+
+## Links Tested
+[Capital Searching]()
+[Country Searching]()
+[Wrong Searching]()
--- api/finder.py
+++ api/finder.py
@@ -1,12 +1,41 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+# from datetime import datetime
+from urllib import parse
+import requests
 
 class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        the_path = self.path
+        url_components = parse.urlsplit(the_path)
+        print(url_components)
+        query_list = parse.parse_qsl(url_components.query)
+        dictionary = dict(query_list)
+        
+        # send request to API
+        if 'capital' in dictionary:
+            capital = dictionary['capital'] # saving to query
+            url ='https://restcountries.com/v3.1/capital/'
+            r = requests.get(url+capital)
+            data = r.json() # to form json!
+            print(data)
+            country = data[0]['name']['common']
+            message = f"{dictionary['capital']} is the capital of {country}"
+        
+        elif 'country' in dictionary:
+            country = dictionary['country']
+            url = 'https://restcountries.com/v3.1/name/'
+            r = requests.get(url + country)
+            data = r.json()
+            capital = data[0]['capital'][0]
+            message = f"The capital of {dictionary['country']} is {capital}"
+        
+        else:
+            message = "You entered something wrong, please enter a country or capital"
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    self.wfile.write(str(date).encode())
-    return
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+        return
